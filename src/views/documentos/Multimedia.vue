bdc3403559a3a0c924839ea57e217c4b78752f05
jmfdz <jm.fdz@icloud.com>
2026-04-12 14:17:39 +0200

    Ampliar apartado de multimedia
    - resumir subtitulado, audiodescripción y lengua de signos
    
    - añadir referencia a la guía de Accesibilidad Digital de la UA

--- src/views/documentos/Multimedia.vue
+++ src/views/documentos/Multimedia.vue
@@ -5,9 +5,66 @@
       herramientas para la evaluación automática de la accesibilidad de documentos Word.
     </p>
 
+    <h2 class="mt-4 mb-3">Percepción de lo audiovisual</h2>
+    <p>
+      Los documentos multimedia están formados por un conjunto de imágenes, sonidos y textos
+      simultáneos, unidos entre sí a través de transiciones. Por su composición es muy difícil
+      diseñar un video o un audio accesible para todas las personas.
+    </p>
+    <p>
+      Sin embargo, el diseño universal no excluye la adaptación, y disponemos de herramientas para
+      asegurar un alto grado de accesibilidad para los documentos multimedia.
+    </p>
+
     <h2 class="mt-4 mb-3">¿Y cómo accede una persona con discapacidad al contenido?</h2>
     <ul>
       <li>Si no puedo escuchar una película, leo los subtítulos.</li>
+    </ul>
+
+    <h2 class="mt-4 mb-3">Subtitulado</h2>
+    <p>
+      El subtitulado es un servicio de apoyo a la comunicación que muestra en pantalla, mediante
+      texto y gráficos, los discursos orales, la información suprasegmental y los efectos sonoros
+      que se producen en cualquier obra audiovisual.
+    </p>
+    <p>Podemos distinguir distintos tipos de subtitulado:</p>
+    <ul>
+      <li>Subtítulos para sordos.</li>
+      <li>Subtítulos para oyentes.</li>
+      <li>Subtítulos en diferido.</li>
+      <li>Subtítulos en directo.</li>
+    </ul>
+
+    <h2 class="mt-4 mb-3">Audiodescripción</h2>
+    <p>
+      Es un servicio de apoyo a la comunicación. Son un conjunto de técnicas y habilidades
+      aplicadas, con objeto de compensar la carencia de captación de la parte visual contenida en
+      cualquier tipo de mensaje.
+    </p>
+    <p>A continuación, se enumeran los pasos de una audiodescripción:</p>
+    <ul>
+      <li>Preparación del guion de la audiodescripción.</li>
+      <li>Audio-introducción.</li>
+      <li>Descripción de los actores, la acción y los escenarios.</li>
+      <li>Locución.</li>
+      <li>Uso del lenguaje.</li>
+      <li>Densidad de la audiodescripción, evitando dentro de lo posible que sea alta.</li>
+    </ul>
+
+    <h2 class="mt-4 mb-3">Lengua de signos</h2>
+    <p>
+      Se tiende a pensar que los lenguajes de signos son similares a los idiomas del país en que se
+      encuentran, pero no es así. Son idiomas totalmente diferentes y cada uno tiene sus propias
+      reglas.
+    </p>
+    <p>
+      Nuestros documentos multimedia deberían estar signados, en la medida de lo posible. Y si no
+      es posible, al menos crear resúmenes con la información más importante en este sistema.
+    </p>
+
+    <h3 class="mt-4 mb-3">Referencia</h3>
+    <ul>
+      <li><a href="https://web.ua.es/es/accesibilidad/documentos-electronicos-accesibles/documentos-multimedia.html" target="_blank" rel="noreferrer">UA: Documentos multimedia</a></li>
     </ul>
   </div>
 </template>
